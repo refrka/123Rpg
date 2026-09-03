138b94e8cd1d9f9cc55e60520f323edf4a0d2049
refkra <la.jernigan88@gmail.com>
2026-09-02 22:03:01 -0700

    Events structure

--- autoloads/Events.gd
+++ autoloads/Events.gd
@@ -3,9 +3,58 @@ extends Node
 
 
 
+var subscriptions: Dictionary[Script, Array]
+
+
 
 
 
 func _ready() -> void:
 
 	process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+
+
+
+
+
+func subscribe(event_script: Script, callback: Callable) -> void:
+
+	if !subscriptions.has(event_script):
+
+		subscriptions[event_script] = []
+
+	subscriptions[event_script].append(callback)
+
+
+
+
+
+func unsubscribe(event_script: Script, callback: Callable) -> void:
+
+	if subscriptions.has(event_script) and subscriptions[event_script].has(callback):
+
+		subscriptions[event_script].erase(callback)
+
+		if subscriptions[event_script].is_empty():
+
+			subscriptions.erase(event_script)
+
+
+
+
+
+func fire(event_script: Script, blackboard: Blackboard = null) -> void:
+
+	var event = event_script.new()
+
+	event.fire(blackboard)
+
+	if subscriptions.has(event_script):
+
+		for callback in subscriptions[event_script]:
+
+			if callback.is_valid():
+
+				callback.call(event)
--- system/event/event.gd
+++ system/event/event.gd
@@ -1,1 +1,11 @@
 @abstract class_name Event extends RefCounted
+
+
+
+var blackboard: Blackboard
+
+
+
+func fire(_blackboard: Blackboard) -> void:
+
+	blackboard = _blackboard
