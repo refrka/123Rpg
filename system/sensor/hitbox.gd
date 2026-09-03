94251380517f882c0d1748a6a884bc1999fda4e5
refkra <la.jernigan88@gmail.com>
2026-09-03 06:51:29 -0700

    Sensor/Hitbox structure

--- system/sensor/hitbox.gd
+++ system/sensor/hitbox.gd
@@ -1,1 +1,101 @@
 class_name Hitbox extends Sensor
+
+
+
+signal hit_detected(entity_node: EntityNode)
+
+
+
+
+var hit_list: Array[Hurtbox]
+
+var ignore_list: Array[EntityNode]
+
+
+var hit_timer:= 0.0
+
+
+
+
+func clear_hit_list() -> void:
+
+	hit_timer = 0.0
+
+	hit_list.clear()
+
+
+
+
+func _start_timer() -> void:
+
+	hit_timer = 3.0
+
+
+
+func _expire_hit() -> void:
+
+	clear_hit_list()
+
+	hit_timer = 0.0
+
+	for area in sensors:
+
+		if area is Hurtbox:
+
+			_try_hit(area)
+
+
+
+
+
+func _try_hit(hurtbox: Hurtbox) -> void:
+
+	if !hurtbox.active or hurtbox.entity == entity or ignore_list.has(hurtbox.entity):
+
+		return
+
+	if hit_list.has(hurtbox):
+
+		return
+
+	_hit(hurtbox)
+
+
+
+
+
+
+func _hit(hurtbox: Hurtbox) -> void:
+
+	hit_list.append(hurtbox)
+
+	hit_detected.emit(hurtbox.entity)
+
+	_start_timer()
+
+
+
+
+
+func _on_area_entered_sensor(area: Area2D) -> void:
+
+	super(area)
+
+	area = area as Hurtbox
+
+	_try_hit(area)
+	
+
+
+
+
+
+func _process(delta: float) -> void:
+
+	if hit_timer > 0.0:
+
+		hit_timer -= delta
+
+		if hit_timer <= 0.0:
+
+			_expire_hit()
--- system/sensor/sensor.gd
+++ system/sensor/sensor.gd
@@ -1,1 +1,164 @@
 class_name Sensor extends Area2D
+
+
+signal entity_entered_sensor(entity_node: EntityNode)
+
+signal entity_exited_sensor(entity_node: EntityNode)
+
+signal sensor_entered_sensor(sensor: Sensor)
+
+signal sensor_exited_sensor(sensor: Sensor)
+
+signal entity_entered_shape(entity_node: EntityNode, local_shape_index: int)
+
+signal entity_exited_shape(entity_node: EntityNode, local_shape_index: int)
+
+
+
+var _initialized:= false
+
+var active:= false
+
+var entity: EntityNode
+
+
+var entities: Array[EntityNode]
+
+var sensors: Array[Sensor]
+
+
+
+
+
+func initialize(_entity: EntityNode = null) -> void:
+
+	if _initialized:
+
+		return
+
+	_initialized = true
+
+	entity = _entity
+
+
+
+
+
+func activate() -> void:
+
+	if active:
+
+		return
+
+	active = true
+
+	_connect_signals()
+
+
+
+
+
+func deactivate() -> void:
+
+	if !active:
+
+		return
+
+	active = false
+
+	_disconnect_signals()
+
+
+
+
+
+func _connect_signals() -> void:
+
+	body_entered.connect(_on_body_entered_sensor)
+
+	body_exited.connect(_on_body_exited_sensor)
+
+	area_entered.connect(_on_area_entered_sensor)
+
+	area_exited.connect(_on_area_exited_sensor)
+
+	body_shape_entered.connect(_on_body_shape_entered_shape)
+
+	body_shape_exited.connect(_on_body_shape_exited_shape)
+
+
+
+func _disconnect_signals() -> void:
+
+	body_entered.disconnect(_on_body_entered_sensor)
+
+	body_exited.disconnect(_on_body_exited_sensor)
+
+	area_entered.disconnect(_on_area_entered_sensor)
+
+	area_exited.disconnect(_on_area_exited_sensor)
+
+
+
+
+
+
+func _on_body_entered_sensor(body: PhysicsBody2D) -> void:
+
+	if body is EntityNode and body != entity:
+
+		entities.append(body)
+
+		entity_entered_sensor.emit(body)
+
+
+
+func _on_body_exited_sensor(body: PhysicsBody2D) -> void:
+
+	if entities.has(body):
+
+		entities.erase(body)
+
+		entity_exited_sensor.emit(body)
+
+
+
+
+
+func _on_area_entered_sensor(area: Area2D) -> void:
+
+	if area is Sensor:
+
+		sensors.append(area)
+
+		sensor_entered_sensor.emit(area)
+
+
+
+
+func _on_area_exited_sensor(area: Area2D) -> void:
+
+	if sensors.has(area):
+
+		sensors.erase(area)
+
+		sensor_exited_sensor.emit(area)
+
+
+
+
+func _on_body_shape_entered_shape(_body_rid: RID, body: Node2D, _body_shape_index: int, local_shape_index: int) -> void:
+
+	if body is EntityNode and body != entity:
+
+		entity_entered_shape.emit(body, local_shape_index)
+
+
+
+
+
+func _on_body_shape_exited_shape(_body_rid: RID, body: Node2D, _body_shape_index: int, local_shape_index: int) -> void:
+
+	if body is EntityNode and body != entity:
+
+		entity_exited_shape.emit(body, local_shape_index)
