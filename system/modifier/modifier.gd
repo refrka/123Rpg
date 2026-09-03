527340dcf8ca14c4866a4bb4a027b9d980e877fa
refkra <la.jernigan88@gmail.com>
2026-09-03 06:52:33 -0700

    Modifier/Tracker structure

--- system/modifier/modifier.gd
+++ system/modifier/modifier.gd
@@ -1,1 +1,130 @@
 class_name Modifier extends RefCounted
+
+
+signal expired
+
+
+enum ModifierType {
+
+	PASSIVE,
+
+	TIMED,
+
+	DECAY,
+
+}
+
+
+
+
+var value: Variant
+
+var modifier_type: ModifierType
+
+var decay_rate:= -1.0
+
+var duration:= -1.0
+
+
+
+
+var _time_alive:= 0.0
+
+
+
+func _is_expired() -> bool:
+
+	match modifier_type:
+
+		ModifierType.TIMED:
+
+			return _time_alive >= duration
+
+		ModifierType.DECAY:
+
+			if value is float:
+
+				return value < 0.001
+
+			elif value is Vector2:
+
+				return value.length_squared() < 0.001
+
+	return false
+
+
+
+
+
+
+
+static func new_passive(_value: Variant) -> Modifier:
+
+	var modifier = Modifier.new()
+
+	modifier.modifier_type = ModifierType.PASSIVE
+
+	modifier.value = _value
+
+	return modifier
+
+
+
+
+static func new_timed(_value: Variant, _duration: float) -> Modifier:
+
+	var modifier = Modifier.new()
+
+	modifier.modifier_type = ModifierType.TIMED
+
+	modifier.value = _value
+
+	modifier.duration = _duration
+
+	return modifier
+
+
+
+static func new_decay(_value: Variant, _decay_rate: float) -> Modifier:
+
+	var modifier = Modifier.new()
+
+	modifier.modifier_type = ModifierType.DECAY
+
+	modifier.value = _value
+
+	modifier.decay_rate = _decay_rate
+
+	return modifier
+
+
+
+
+
+static func get_modifier() -> Modifier:
+
+	return null
+
+
+
+
+func _tick(delta: float) -> void:
+
+	_time_alive += delta
+
+	if decay_rate > 0.0:
+
+		value *= clampf(1.0 - decay_rate * delta, 0.0, 1.0)
+
+	if _is_expired():
+
+		expired.emit()
+
+
+
+
+
+
+
+
+	
--- system/modifier/modifier_tracker.gd
+++ system/modifier/modifier_tracker.gd
@@ -1,1 +1,79 @@
 class_name ModifierTracker extends Resource
+
+
+
+
+
+var modifiers: Array[Modifier]
+
+
+
+func add_modifier(modifier: Modifier) -> void:
+
+	modifiers.append(modifier)
+
+	modifier.expired.connect(_on_modifier_expired.bind(modifier))
+
+
+
+
+
+func remove_modifier(modifier: Modifier) -> void:
+
+	modifiers.erase(modifier)
+
+
+
+
+
+
+func get_total_float_value() -> float:
+
+	var total:= 0.0
+
+	for modifier in modifiers:
+
+		total += modifier.value
+
+	return total
+
+
+
+func get_total_vector_value() -> Vector2:
+
+	var total:= Vector2.ZERO
+
+	for modifier in modifiers:
+
+		total += modifier.value
+
+	return total
+
+
+
+
+func get_total_multiplier_value() -> float:
+
+	var total:= 1.0
+
+	for modifier in modifiers:
+
+		total *= modifier.value
+
+	return total
+
+
+
+func tick_modifiers(delta: float) -> void:
+
+	for modifier in modifiers:
+
+		modifier._tick(delta)
+
+
+
+
+
+func _on_modifier_expired(modifier: Modifier) -> void:
+
+	modifiers.erase(modifier)
