7da4e2bc34687b7446b5e64c1009189dd08e1751
refkra <la.jernigan88@gmail.com>
2026-09-02 21:05:57 -0700

    Thief. Collision shapes on Player and Thief.

--- game_root.gd
+++ game_root.gd
@@ -7,11 +7,19 @@ func _ready() -> void:
 
 	var player = load("res://player/player.tscn").instantiate()
 
+	var thief = load("res://entity/characters/enemies/thief.tscn").instantiate()
+
 	var location = Scenes.get_location()
 
 	location.add_entity_node(player, Vector2(200.0, 200.0))
 
+	location.add_entity_node(thief, Vector2(400.0, 200.0))
+
 	player._initialize()
 
 	player._activate()
+
+	thief._initialize()
+
+	thief._activate()
 
